ebbcb924d5d9877c8193a971df3f9541b65c4d84
Olivia Kasten <odkasten@gmail.com>
2018-08-28 23:32:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,8 +35,7 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop()
-  return array
+  return array.pop()
 }
 
 function removeElementFromEndOfArray(array){
