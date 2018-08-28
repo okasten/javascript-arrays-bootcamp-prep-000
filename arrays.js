9a0ebc0d77698cb405f2b63ca729eac068363d65
Olivia Kasten <odkasten@gmail.com>
2018-08-28 21:48:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element)
+  var ar = array.unshift(element)
 }
