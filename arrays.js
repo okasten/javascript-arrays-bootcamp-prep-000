b00f86187b55a5b46e9e00fdb5e04c898adb12b1
Olivia Kasten <odkasten@gmail.com>
2018-08-28 23:19:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array
 }
 
+function addElementToEndOfArray(array, element){
+  var ar = [...array, element]
+  return ar
+}
